83d0c44421cb880ba89ea19beda4e51eb4f9e6f2
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2026-04-14 10:39:19 +0700

    Commit 2: Menyiapkan variabel connection string ke SQL Server

--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Data;
-using System.Data.SqlClient; //
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
+        // Menambahkan variabel koneksi dan alamat database
+        private readonly SqlConnection conn;
+        private readonly string connectionString = @"Data Source=LAPTOP-7SOCNODM\ANDHIKA1; Initial Catalog=DBAkademikADO; Integrated Security=True";
+
         public Form1()
         {
-            InitializeComponent(); //
+            InitializeComponent();
+            // Inisialisasi koneksi
+            conn = new SqlConnection(connectionString);
         }
     }
 }
