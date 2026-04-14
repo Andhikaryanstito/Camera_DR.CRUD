3500834672d1cf5860cbadd2aa0475711694338d
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2026-04-14 10:35:57 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient; //
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); //
+        }
+    }
+}
