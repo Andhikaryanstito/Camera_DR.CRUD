ef2f79714f491bb15a9e2bd8efa1039a07b280fb
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2026-06-16 23:26:08 +0700

    Menyelesaikan routing navigasi Form1 dan membersihkan seluruh dependensi error

--- FormDashboard.cs
+++ FormDashboard.cs
@@ -43,9 +43,11 @@ namespace CRUDMahasiswaADO
             cmbTipe.ValueMember = "Value";
             cmbTipe.SelectedIndex = 0;
             isInitializing = false;
+
+            loadDataChart();
         }
 
-        // 10. Tambahkan method loadDataChart() (Bagian Awal)
+        // 10. Tambahkan method loadDataChart()
         public void loadDataChart()
         {
             chartProdi.Series.Clear();
@@ -70,15 +72,86 @@ namespace CRUDMahasiswaADO
                 {
                     dt = dbLogic.getAllDataChart();
                 }
+
+                SeriesChartType tipe = (SeriesChartType)cmbTipe.SelectedValue;
+                if (tipe == SeriesChartType.Column)
+                {
+                    Series s = new Series("Mahasiswa");
+                    s.ChartType = SeriesChartType.Column;
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string prodi = row["NamaProdi"].ToString();
+                        int jumlah = Convert.ToInt32((long)row["JmlhMhs"]);
+                        s.Points.AddXY(prodi, jumlah);
+                    }
+                    chartProdi.Series.Add(s);
+                }
+                else
+                {
+                    Series s = new Series("Jumlah Mahasiswa");
+                    s.ChartType = tipe;
+                    s.IsValueShownAsLabel = true;
+                    s.Label = "#VAL";
+                    s.LegendText = "#VALX";
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string prodi = row["NamaProdi"].ToString();
+                        int jumlah = Convert.ToInt32((long)row["JmlhMhs"]);
+                        s.Points.AddXY(prodi, jumlah);
+                    }
+                    chartProdi.Series.Add(s);
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Gagal load data: " + ex.Message);
             }
+
+            Title title = new Title("Jumlah Mahasiswa per Program Studi", Docking.Top, new Font("Arial", 14, FontStyle.Bold), Color.DarkBlue);
+            chartProdi.Titles.Add(title);
+            Legend legend = new Legend("MainLegend");
+            legend.Docking = Docking.Right;
+            chartProdi.Legends.Add(legend);
         }
 
+        // 11. Tambahkan Event SelectedValueChanged pada cmbTipe
+        private void cmbTipe_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (isInitializing)
+                return;
+
+            if (button == 1)
+            {
+            }
+            else
+            {
+                loadDataChart();
+            }
+        }
+
+        // 12. Tambahkan Event Click pada button
+        // a. Button Load
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            button = 1;
+            loadDataChart();
+        }
+
+        // b. Button Reset
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            button = 0;
+            loadDataChart();
+        }
+
+        // c. Button Data Mahasiswa
         private void btnDataMahasiswa_Click(object sender, EventArgs e)
         {
+            // Perbaikan: Mengganti DataMahasiswa menjadi Form1 agar sesuai project kamu
+            Form1 frm1 = new Form1();
+            frm1.Show();
+            this.Hide();
         }
 
         private void chart1_Click(object sender, EventArgs e)
